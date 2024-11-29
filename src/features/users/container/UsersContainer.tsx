c81e556b8f5a6585a4492db01249d2ca6963d65f
kwn <kwn1125dev@gmail.com>
2024-11-29 11:34:50 +0900

    feat: add "Add user" button linking to "/users/new"

--- src/features/users/container/UsersContainer.tsx
+++ src/features/users/container/UsersContainer.tsx
@@ -1,12 +1,18 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import { UsersTable } from "../presenter/UsersTable";
 
 export const UsersContainer = () => {
   return (
     <>
-      <Typography component="h1" variant="h3">
-        Users
-      </Typography>
+      <Box display="flex" alignItems="center">
+        <Typography component="h1" variant="h3" flexGrow={1}>
+          Users
+        </Typography>
+        <Button component={Link} to="/users/new" variant="contained">
+          Add user
+        </Button>
+      </Box>
       <UsersTable />
     </>
   );
